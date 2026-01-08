8d7e3f10f12a363a63ed5a98cfe3bbf3609b8a84
Sebastian Musial <dev.sebas.musial@gmail.com>
2026-01-08 08:53:51 +0100

    Fix bug in class Snake and add objects + methods other class in game class

---
+++ snake-the-game/Game.gd
@@ -0,0 +1,127 @@
+extends Node2D
+class_name Game
+#Zmienna typu Timer odwołującą się do node TurnTimer
+@onready var Turn_Timer: Timer = $TurnTimer
+
+@export var board_width: int = 20
+@export var board_height: int = 20
+@onready var tile_map: TileMap = $TileMap
+
+#Zmienne do odbioru pixeli z pliku graficznego przez TileMap
+const SOURCE_ID := 0
+const ATLAS_BG := Vector2i(3, 3)
+const ATLAS_SNAKE := Vector2i(0, 0)
+const ATLAS_FRUIT := Vector2i(0, 3)
+const ATLAS_WALL := Vector2i(3, 0)
+
+#Tworzenie obiektów z moich klas
+var snake: Snake
+var fruit: Fruit
+var board: Board
+var rules: Game_Rules
+
+
+var wall_body: Array[Vector2i] = []
+
+var rng := RandomNumberGenerator.new()
+
+enum directions { UP, DOWN, LEFT, RIGHT }
+
+#Metody
+func draw_board() -> void:
+	tile_map.clear()
+	
+	for y in range(board_height):
+		for x in range(board_width):
+			tile_map.set_cell(0, Vector2i(x, y), SOURCE_ID, ATLAS_BG)
+
+func init_wall() -> void:
+	wall_body.clear()
+	for X in range(board_width):
+		wall_body.append(Vector2i(X, 0))
+		wall_body.append(Vector2i(X, board_height - 1))
+	for Y in range(board_height):
+		wall_body.append(Vector2i(0, Y))
+		wall_body.append(Vector2i(board_width - 1, Y))
+	wall_body.append(Vector2i(board_width, board_height))	
+	
+func draw_wall() -> void:	#ściany są rysowane poza planszą - w grze nie widać ale matematyka się nie zgadza
+	for segment in wall_body:
+		tile_map.set_cell(0, segment, SOURCE_ID, ATLAS_WALL)
+
+#Aktualnie rysuje się tylko pierwszy segment węża
+func init_snake() -> void:
+	snake.get_body().clear()
+	snake.get_body().append(Vector2i(int(board_width / 2), int(board_height / 2)))
+
+func draw_snake() -> void:
+	var body := snake.get_body()
+	for segment in body:
+		tile_map.set_cell(0, segment, SOURCE_ID, ATLAS_SNAKE)
+
+
+#Narazie testowo głowa się przesuwa w prawo 
+func move_snake() -> void:
+	var head := snake.get_body()[0]
+	var new_head := head + Vector2i(1, 0) # w prawo
+	
+	snake.get_body().insert(0, new_head)	#Nowa głowa
+	snake.get_body().remove_at(snake.get_body().size() - 1)			#Kasowanie ogona - ostatniego elementu
+
+
+#Owoc ma już swoj spawn z RNG dla pozycji [bug:owov moze sie generowac w scianie]
+func init_fruit() -> void:
+	while true:
+		var pos := Vector2i(rng.randi_range(0, board_width - 1), rng.randi_range(0, board_height - 1))
+		if pos not in snake.get_body():
+			fruit.set_body(pos)
+			return
+	
+func draw_fruit() -> void:
+	tile_map.set_cell(0, fruit.get_body(), SOURCE_ID, ATLAS_FRUIT)	
+	
+#Sterowanie - TEST DZIALA
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed:
+		match event.keycode:
+			Key.KEY_UP: print("Gora")
+			Key.KEY_W: print("Gora")
+			Key.KEY_DOWN: print("Dol")
+			Key.KEY_S: print("Dol")
+			Key.KEY_LEFT: print("Lewo")
+			Key.KEY_A: print("Lewo")
+			Key.KEY_RIGHT: print("Prawo")
+			Key.KEY_D: print("Prawo")
+
+	
+#!!!!TUTAJ JUZ MECHANIKA JAK WSZYSTKO PRZEPLYWA PRZEZ GRE!!!#
+func _ready():
+	rng.randomize()	
+	snake = Snake.new()
+	fruit = Fruit.new()
+	board = Board.new()
+	rules = Rules_Normal.new()
+	
+	init_snake()
+	init_fruit()
+	init_wall()
+	draw_board()
+	draw_wall()
+	draw_snake()
+	draw_fruit()
+	Turn_Timer.wait_time = 1.0   # 1000 ms
+	Turn_Timer.one_shot = false  # Timer powtarzalny - Gdyby True to wywołał by się tylko raz
+	Turn_Timer.start()           #Start odliczania czasu
+
+func _next_turn() -> void:
+	move_snake()
+	draw_board()
+	draw_wall()
+	draw_snake()
+	draw_fruit()
+	print("Next turn")
+	
+
+#Sygnał - metoda łącząca Game z timer
+func _on_Turn_Timer_timeout() -> void:
+	_next_turn()
---
+++ snake-the-game/Snake_Class.gd
@@ -0,0 +1,57 @@
+extends Player
+class_name Snake
+
+
+
+#Enkapsulacja zgodnie z konwencja OOP czyli stosujemy _<nazwa zmiennej>
+#Private - konwencja ponieważ GDscript pomimo obiektowości tego nie oferuje
+var _snake_body: Array[Vector2i] = []
+var _dir: directions
+var _grows:= false
+
+#Public
+#Get
+func get_body() -> Array[Vector2i]:
+	return _snake_body 
+	
+#Set
+#Hybryda z szablonu C++ set dir + try set dir
+func set_dir(N_dir) -> void:
+	if not is_opposite(_dir, N_dir):
+		_dir = N_dir
+	
+func set_grows(N_grows) -> void:
+	_grows = N_grows
+	
+#Metody
+func head() -> Vector2i:
+	return _snake_body[0] if _snake_body.size() > 0 else Vector2i.ZERO
+
+func reset(start_X, start_Y, N_dir = direction.UP) -> void:
+	_snake_body.clear()
+	_snake_body.append(Vector2i(start_X, start_Y))
+	_dir = N_dir #Czy to jest konieczne z argumentu?
+	_grows = false
+	#reset_score - musze stworzyc dla klasy player
+
+func eat() -> void:
+	if not _snake_body.is_empty():
+		var last_element: Vector2i = _snake_body[-1]
+		_snake_body.append(last_element)
+
+#func occupies(vec2i) -> const bool
+#Metoda ze szkicu C++ ale na tą chwile nie potrzebna bo wszystko mam w Vector2i
+
+func grows() -> void:
+	set_grows(true) 
+	
+#Kolizje - Mechanika w kodzie pomimo że godot udostępnia swoje node do kolicji postaci 2D 
+#Metoda ze szkicu C++ - 3 w 1
+func collision() -> void: 
+	var head_snake_pos: Vector2i = snake_body[0] 
+#Snake = Wall 
+	if head_snake_pos not in wall_body: print("Brak kolizji_W") 
+#Snake = Snake [Brak wyjątku gdzie głowa to tez ciało]
+	if head_snake_pos in snake_body.slice(1): print("Kolizja_S") 
+#Snake = Fruit 
+	if head_snake_pos == fruit_body: print("Am...Am...")
